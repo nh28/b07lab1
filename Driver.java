304b1ccaa1e2f5475090cf15ef90acbdf2619d76
nh28 <nharsch9@outlook.com>
2023-09-28 12:47:29 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,89 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+		// test the empty parameter constuctor 
+			Polynomial p = new Polynomial();
+			System.out.println(p.evaluate(3));
+		
+		// test creating a polynomial with parameters (p1.length > p2.length)
+			double [] c1 = {6,2, 5};
+			int [] c11 = {0, 1, 3};
+			Polynomial p1 = new Polynomial(c1, c11);
+		
+			// create another polynomial
+			double [] c2 = {3, 4};
+			int [] c22 = {1, 2};
+			Polynomial p2 = new Polynomial(c2, c22);
+
+			// test the add function
+			Polynomial s1 = p1.add(p2);
+
+			// test the evaluate function
+			System.out.println("s1(0.1) = " + s1.evaluate(0.1));
+
+			// test the hasRoot function
+			if(s1.hasRoot(1))
+				System.out.println("1 is a root of s");
+			else
+				System.out.println("1 is not a root of s");
+
+		// test creating a polynomial with parameters (p2.length > p1.length)
+			double [] a1 = {3, 4};
+			int [] a11 = {1, 2};
+			Polynomial m1 = new Polynomial(a1, a11);
+		
+			// create another polynomial
+			double [] a2 = {-6,-2, -5};
+			int [] a22 = {0, 1, 3};
+			Polynomial m2 = new Polynomial(a2, a22);
+
+			// test the add function
+			Polynomial s2 = m1.add(m2);
+
+			// test the evaluate function
+			System.out.println("s2(0.1) = " + s2.evaluate(0.1));
+
+		// test creating a polynomial with parameters (p2.length = p1.length)
+			double [] b1 = {-3, -4};
+			int [] b11 = {1, 2};
+			Polynomial n1 = new Polynomial(b1, b11);
+		
+			// create another polynomial
+			double [] b2 = {-6,-2};
+			int [] b22 = {0, 1};
+			Polynomial n2 = new Polynomial(b2, b22);
+
+			// test the add function
+			Polynomial s3 = n1.add(n2);
+
+			// test the evaluate function
+			System.out.println("s3(0.1) = " + s3.evaluate(0.1));
+
+		// test the multiply function
+			// positive * positive
+			Polynomial t1 = p1.multiply(p2);
+			System.out.println("t1(1) = " + t1.evaluate(1));
+
+			// positive * negative
+			Polynomial t2 = m1.multiply(m2);
+			System.out.println("t2(1) = " + t2.evaluate(1));
+
+			// negative * negative
+			Polynomial t3 = n1.multiply(n2);
+			System.out.println("t3(1) = " + t3.evaluate(1));			
+
+		// test File constructor
+			File myObj = new File("test.txt");
+			Polynomial u = new Polynomial(myObj);
+			System.out.println("u(0.1) = " + u.evaluate(0.1));
+
+		// test saveToFile
+			// starts with a positive
+			s1.saveToFile("test1.txt");
+			// starts with a negative
+			s3.saveToFile("test2.txt");
+		
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,38 +1,145 @@
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Polynomial
 {
 	double[] coefficients;
+	int[] exponents;
 	public Polynomial()
 	{
 		coefficients = new double[1];
 		coefficients[0] = 0;
+
+		exponents = new int[1];
+		exponents[0] = 0;
 	}
-	public Polynomial(double[] array)
+	public Polynomial(double[] array1, int[] array2)
 	{
-		coefficients = new double[array.length];
-		for(int i = 0; i < array.length; i++)
+		coefficients = new double[array1.length];
+		exponents = new int[array2.length];
+		for(int i = 0; i < array1.length; i++)
 		{
-			coefficients[i] = array[i];
+			coefficients[i] = array1[i];
+			exponents[i] = array2[i];
 		}
 	}
-	public Polynomial add(Polynomial poly)
+
+	public Polynomial(File file)
 	{
-		if (poly.coefficients.length > coefficients.length)
+		try{
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		String poly = input.readLine();
+		String[] splitPoly = poly.split("[+-]", 0);
+		int counter = 0;
+		
+		coefficients = new double[splitPoly.length];
+		exponents = new int[splitPoly.length];
+		
+		int i = 0;
+		int j = 1;
+		if (splitPoly[0] != "")
 		{
-			for(int i = 0; i < coefficients.length; i++)
+			String[] first = splitPoly[0].split("x", 0);
+			coefficients[i] = Double.parseDouble(first[0]);
+			if (first.length > 1)
 			{
-			poly.coefficients[i] += coefficients[i];
+				exponents[i] = Integer.parseInt(first[1]);
+				counter+=2;
 			}
-
-			return poly;
+			else
+			{
+				exponents[i] = 0;
+			}
+			counter++;
+			i++;
+				
 		}
-
-		for(int i = 0; i < poly.coefficients.length; i++)
+		while(j < splitPoly.length)
 		{
-
-			coefficients[i] += poly.coefficients[i];
+			String[] elements = splitPoly[j].split("x", 0);
+			if (poly.substring(counter, counter + 1).equals("-"))
+			{
+				coefficients[i] = Double.parseDouble("-" + elements[0]);
+			}
+			else
+			{
+				coefficients[i] = Double.parseDouble(elements[0]);
+			}
+			counter+=2;
+			if(elements.length == 1)
+			{
+				exponents[i] = 0;
+				counter--;
+			}
+			else
+			{
+				exponents[i] = Integer.parseInt(elements[1]);
+				counter ++;
+			}
+			counter++;
+			i++;
+			j++;
 			
 		}
-		return this;
+		}
+		catch(FileNotFoundException ex){}
+		catch(IOException e){}
+		
+		
+	}
+
+	public Polynomial add(Polynomial poly)
+	{
+		int maxExpo = 0;
+		int minExpo = 0;
+		
+		for(int a = 0; a < exponents.length; a++)
+		{
+			if (maxExpo < exponents[a])
+			{
+				maxExpo = exponents[a];
+			}
+			if (minExpo > exponents[a])
+			{
+				minExpo = exponents[a];
+			}
+		}
+		for(int b = 0; b < poly.exponents.length; b++)
+		{
+			if (maxExpo < poly.exponents[b])
+			{
+				maxExpo = poly.exponents[b];
+			}
+			if (minExpo > poly.exponents[b])
+			{
+				minExpo = poly.exponents[b];
+			}
+		}
+		
+
+		double[] coeffNew = new double[maxExpo - minExpo + 1];
+		int[] exponentNew = new int[maxExpo - minExpo + 1];
+		for(int i = 0; i < exponentNew.length; i++)
+		{
+			exponentNew[i] = i;
+		}
+		
+		for(int j = 0; j < coefficients.length; j++)
+		{
+			coeffNew[exponents[j]] += coefficients[j];
+		}
+
+		for(int k = 0; k < poly.coefficients.length; k++)
+		{
+			coeffNew[poly.exponents[k]] += poly.coefficients[k];
+		}
+
+		Polynomial added = new Polynomial(coeffNew, exponentNew);
+		return added;
 		 
 	}
 	public double evaluate(double x)
@@ -40,12 +147,66 @@ public class Polynomial
 		double eval = 0;
 		for(int i = 0; i < coefficients.length; i++)
 		{
-			eval += coefficients[i] * Math.pow(x, i);
+			eval += coefficients[i] * Math.pow(x, exponents[i]);
 		}
 		return eval; 
 	}
 	public boolean hasRoot(double x)
 	{
 		return evaluate(x) == 0;
+	}
+
+	public Polynomial multiply(Polynomial poly)
+	{
+		Polynomial newPoly = new Polynomial();
+		
+		for(int j = 0; j < coefficients.length; j++)
+		{
+			int size = poly.coefficients.length;
+			double[] thisCoeff = new double[size];
+			int[] thisExpo = new int[size];
+
+			for(int k = 0; k < size; k++)
+			{
+				thisCoeff[k] = coefficients[j] * poly.coefficients[k];
+				thisExpo[k] = poly.exponents[k] + exponents[j];
+			}
+			Polynomial temp = new Polynomial(thisCoeff, thisExpo);
+			newPoly = newPoly.add(temp);
+		}
+
+		return newPoly;
+	}
+
+	public void saveToFile(String fileName)
+	{
+		try{
+		String polynomial = "";
+		for(int i = 0; i < coefficients.length; i++)
+		{
+			double coeff = coefficients[i];
+			int expo = exponents[i];
+			if (coeff > 0)
+			{
+				polynomial += "+" + String.valueOf(coefficients[i]);
+			}
+			else
+			{
+				polynomial += String.valueOf(coefficients[i]);
+			}
+			if (expo > 0)
+			{
+				polynomial += "x" + String.valueOf(expo);
+			}
+			
+		} 
+		PrintStream output = new PrintStream(fileName);
+		if(polynomial.substring(0, 1).equals("+"))
+			output.println(polynomial.substring(1));
+		else		
+			output.println(polynomial.substring(1));
+		output.close();
+		}
+		catch(FileNotFoundException ex){}
 	}
 }
